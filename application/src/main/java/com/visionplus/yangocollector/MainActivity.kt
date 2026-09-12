7d93eefa7aaf95d2263d3771cfb64b8d6bbcce65
Alain229 <77niala@gmail.com>
2026-09-12 16:44:21 +0000

    Add button to open accessibility settings

--- application/src/main/java/com/visionplus/yangocollector/MainActivity.kt
+++ application/src/main/java/com/visionplus/yangocollector/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.visionplus.yangocollector
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.btnEnableAccessibility)
+        button.setOnClickListener {
+            val intent = Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)
+            startActivity(intent)
+        }
     }
 }
