06eb9f3c05369fff22950574f5322b5b8f826c1b
Alain229 <77niala@gmail.com>
2026-09-13 14:10:10 +0000

    Update YangoAccessibilityService.kt

--- application/src/main/java/com/visionplus/yangocollector/YangoAccessibilityService.kt
+++ application/src/main/java/com/visionplus/yangocollector/YangoAccessibilityService.kt
@@ -1,6 +1,7 @@
 package com.visionplus.yangocollector
 
 import android.accessibilityservice.AccessibilityService
+import android.accessibilityservice.AccessibilityServiceInfo
 import android.content.Context
 import android.view.accessibility.AccessibilityEvent
 
@@ -8,8 +9,16 @@ class YangoAccessibilityService : AccessibilityService() {
 
     override fun onServiceConnected() {
         super.onServiceConnected()
+
+        val info = AccessibilityServiceInfo()
+        info.eventTypes = AccessibilityEvent.TYPES_ALL_MASK
+        info.feedbackType = AccessibilityServiceInfo.FEEDBACK_GENERIC
+        info.flags = AccessibilityServiceInfo.DEFAULT or AccessibilityServiceInfo.FLAG_REPORT_VIEW_IDS
+        info.notificationTimeout = 100
+        serviceInfo = info
+
         val prefs = getSharedPreferences("yango_collector_prefs", Context.MODE_PRIVATE)
-        prefs.edit().putString("service_status", "connecte").apply()
+        prefs.edit().putString("service_status", "connecte_v2").apply()
     }
 
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
